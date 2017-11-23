f43145ea6a2f5d3e26cb709b76349d533e30a552
sergio_kag <sergio_kagiema@live.com>
2017-11-23 15:29:27 +0200

    Setting the structure of the project

---
+++ components/shared/sidenav.php
@@ -0,0 +1,8 @@
+<div id="nav">
+	<ul>
+		<li><a href="#1" id="Home"><span class="stm">Home</span><img src="assets/images/home.png" class="icom"></a></li>
+		<li><a href="#2" id="Log"><span class="stm">LogIn</span><img src="assets/images/login.png" class="icom"></a></li>
+		<li><a href="#3" id="Guide"><span class="stm">Guide</span><img src="assets/images/guide.png" class="icom"></a></li>
+		<li><a href="#4" id="Game"><span class="stm">Write!</span><img src="assets/images/edit.png" class="icom"></a></li>
+	</ul>
+</div>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Storyteller</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<?php include('components/shared/header.php');?>
+	
+	<div id="wrap">
+		<?php include('components/shared/sidenav.php');?>
+
+		<div id="main">
+			<div class="home">
+				<h1>Welcome !</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim pariatur aliquid a nemo soluta dignissimos minus accusamus porro natus autem cum error odio exercitationem quidem, veritatis, itaque quas, laudantium ipsam eveniet dolore assumenda est ex doloribus omnis. Doloremque placeat, veniam voluptatibus sit esse, nostrum perferendis in labore nisi provident similique, saepe accusamus officiis vero architecto, debitis eveniet magnam corporis sapiente?</p>
+			</div>
+		</div>
+	</div>
+	<span id="version">Beta v_1.0</span>
+	
+	<!-- js script go before the </body> -->
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+	<script src="assets/js/main.js"></script>
+</body>
+</html>
