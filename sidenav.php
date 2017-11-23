7eb49da71956f5601b9b3c9b83e3423c73930867
Dustin <dustin.gogoll@googlemail.com>
2017-11-23 05:24:27 +0100

    Media queries and new icons added

--- sidenav.php
+++ sidenav.php
@@ -1,8 +1,8 @@
 <div id="nav">
 	<ul>
-		<li><a href="#" id="Home">Home</a></li>
-		<li><a href="#" id="Log">LogIn</a></li>
-		<li><a href="#" id="Guide">Guide</a></li>
-		<li><a href="#" id="Game">Write!</a></li>
+		<li><a href="#1" id="Home"><span class="stm">Home</span><img src="icons/home.png" class="icom"></a></li>
+		<li><a href="#2" id="Log"><span class="stm">LogIn</span><img src="icons/login.png" class="icom"></a></li>
+		<li><a href="#3" id="Guide"><span class="stm">Guide</span><img src="icons/guide.png" class="icom"></a></li>
+		<li><a href="#4" id="Game"><span class="stm">Write!</span><img src="icons/edit.png" class="icom"></a></li>
 	</ul>
 </div>
