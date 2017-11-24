3a638a498c66a47d02d150c743bd21896e44b392
Dustin <dustin.gogoll@googlemail.com>
2017-11-24 17:53:11 +0100

    Added a flat file solution for writing new parts and add them to the page

---
+++ components/writing/basicWritingSave.php
@@ -0,0 +1,23 @@
+<?php
+$directory = "posts/";
+$filecount = 0;
+$files = glob($directory . "*");
+if ($files){
+ $filecount = count($files);
+}
+echo "There were $filecount files";
+if(isset($_POST['write'])) {
+    $data = $_POST['write'];
+    $ret = file_put_contents('posts/file'.$filecount.'.txt', $data);
+    if($ret === false) {
+        die('There was an error writing this file');
+    }
+    else {
+        echo "$ret bytes written to file";
+    }
+}
+else {
+   die(/n'no post data to process');
+}
+
+?>
--- pages/write.php
+++ pages/write.php
@@ -1,5 +1,23 @@
 <section id="writeSection" class="panel-section">
+<?php
+$directory = "componets/writing/posts/";
+$filecount = 0;
+$files = glob($directory . "*");
+if ($files){
+ $filecount = count($files);
+}
 
+for($i=0; $i-1 <= $filecount; $i++){
+  echo "<div>";
+  echo "<p>";
+  include("components/writing/posts/file".$i.".txt");
+  echo "</p>";
+  echo "</div>";
+}
+?>
   <p>This is the write page</p>
-
+<form action="components/writing/basicWritingSave.php" method="post">
+  <textarea name="write" cols="30" rows="10"></textarea>
+  <input type="submit" name="submit" value="Send Text">
+</form>
 </section>
